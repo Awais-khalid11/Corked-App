06b5f7ddae4a1e4af601147a58b0290328b0edef
ZayanMustafa95 <iamzayan10@gmail.com>
2025-07-10 16:12:54 +0500

    changes

--- src/routers/index.jsx
+++ src/routers/index.jsx
@@ -4,6 +4,9 @@ import Dashboard from "@pages/Dashboard";
 import Settings from "@pages/Settings";
 import MainLayouts from "@layouts/MainLayouts";
 import NotFound from "@pages/NotFound";
+import Dashboard from "@pages/Dashboard"
+import WineLevel from "../pages/WineLevel";
+import LogLocationBreakdown from "../pages/LogLocationBreakdwon";
 
 const Routers = () => {
   return (
@@ -12,6 +15,7 @@ const Routers = () => {
         <Route index element={<Dashboard />} />
         <Route path="settings" element={<Settings />} />
         <Route path="wine-level" element={<WineLevel />} />
+        <Route path="log-location-breakdown" element={<LogLocationBreakdown />} />
 
       </Route>
       <Route path="*" element={<NotFound />} />
