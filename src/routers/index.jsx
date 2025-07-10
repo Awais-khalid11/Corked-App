431a85c56227e0e057a855edf9211249e8f6d234
Awais-khalid11 <awaiskhalid523@gmail.com>
2025-07-10 16:58:36 +0500

    engagment and bench page

--- src/routers/index.jsx
+++ src/routers/index.jsx
@@ -1,9 +1,11 @@
-import { Navigate, Route, Routes } from "react-router-dom";
 import Settings from "@pages/Settings";
-import MainLayout from "@layouts/MainLayout";
 import NotFound from "@pages/NotFound";
 import Dashboard from "@pages/Dashboard"
 import WineLevel from "../pages/WineLevel";
+import MainLayout from "@layouts/MainLayout";
+import BenchMarking from "../pages/BenchMarking";
+import EngagmentSummary from "../pages/EngagmentSummary";
+import { Navigate, Route, Routes } from "react-router-dom";
 
 const Routers = () => {
   return (
@@ -12,11 +14,15 @@ const Routers = () => {
         <Route index element={<Dashboard />} />
         <Route path="settings" element={<Settings />} />
         <Route path="wine-level" element={<WineLevel />} />
+        <Route path="engagment-summary" element={<EngagmentSummary />} />
+        <Route path="bench-marking" element={<BenchMarking />} />
+
 
       </Route>
       <Route path="*" element={<NotFound />} />
     </Routes>
   );
 };
+
 
 export default Routers;
